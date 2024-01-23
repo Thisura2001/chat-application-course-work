78e44d5ee82d8ba5f7b611aec0d0632f03a2cc01
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-01-23 15:37:59 +0530

    client ui

--- src/main/java/lk/ijse/Controller/ClientController.java
+++ src/main/java/lk/ijse/Controller/ClientController.java
@@ -1,17 +1,32 @@
 package lk.ijse.Controller;
 
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import lk.ijse.Controller.Client.Client;
 
 public class ClientController {
-    public void btnSendOnAction(ActionEvent actionEvent) {
-    }
+    @FXML
+    private AnchorPane emojiAnchorpane;
 
-    public void btnImageOnAction(ActionEvent actionEvent) {
-    }
+    @FXML
+    private GridPane emojiGridpane;
 
-    public void btnEmojiOnAction(ActionEvent actionEvent) {
-    }
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private ScrollPane scrollPane;
+
+    @FXML
+    private JFXTextField txtMsgArea;
+
+    @FXML
+    private VBox vBox;
 
     public void writeMessage(String message) {
     }
@@ -20,5 +35,14 @@ public class ClientController {
     }
 
     public void setClient(Client client) {
+    }
+
+    public void btnImageOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnImojiOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSendOnAction(ActionEvent actionEvent) {
     }
 }
