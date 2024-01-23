37413f714640bbb191c07457e21f52041a576394
Thisura2001 <141469673+Thisura2001@users.noreply.github.com>
2024-01-23 15:41:14 +0530

    client

--- src/main/java/lk/ijse/Controller/ClientController.java
+++ src/main/java/lk/ijse/Controller/ClientController.java
@@ -3,13 +3,39 @@ package lk.ijse.Controller;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import lk.ijse.Controller.Client.Client;
 
-public class ClientController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientController implements Initializable {
+    private final String[] emojis = {
+            "\uD83D\uDE00", // 😀
+            "\uD83D\uDE01", // 😁
+            "\uD83D\uDE02", // 😂
+            "\uD83D\uDE03", // 🤣
+            "\uD83D\uDE04", // 😄
+            "\uD83D\uDE05", // 😅
+            "\uD83D\uDE06", // 😆
+            "\uD83D\uDE07", // 😇
+            "\uD83D\uDE08", // 😈
+            "\uD83D\uDE09", // 😉
+            "\uD83D\uDE0A", // 😊
+            "\uD83D\uDE0B", // 😋
+            "\uD83D\uDE0C", // 😌
+            "\uD83D\uDE0D", // 😍
+            "\uD83D\uDE0E", // 😎
+            "\uD83D\uDE0F", // 😏
+            "\uD83D\uDE10", // 😐
+            "\uD83D\uDE11", // 😑
+            "\uD83D\uDE12", // 😒
+            "\uD83D\uDE13"  // 😓
+    };
     @FXML
     private AnchorPane emojiAnchorpane;
 
@@ -44,5 +70,10 @@ public class ClientController {
     }
 
     public void btnSendOnAction(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
